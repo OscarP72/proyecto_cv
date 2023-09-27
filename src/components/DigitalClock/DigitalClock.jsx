e2f3a264986452c72780587646f87c6fd847bf4b
UsuarioM <oskitarpereal@gmail.com>
2023-09-27 14:12:47 +0200

    primer commit

---
+++ src/components/DigitalClock/DigitalClock.jsx
@@ -0,0 +1,20 @@
+import "./digitalClock.css";
+import React, {useEffect, useState} from "react";
+
+const DigitalClock = () => {
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString());
+    }, 1000);
+  }, []);
+  return (
+    <div className="digitalClock">
+      <h2>{clockState}</h2>
+    </div>
+  );
+};
+
+export default DigitalClock;
